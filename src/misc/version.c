95b9009df493b4de9df77522c5bd394598c6a089
ayush7788 <ayush7788899@gmail.com>
2021-03-29 18:12:53 +0530

    adding licensing details

--- src/misc/version.c
+++ src/misc/version.c
@@ -3,5 +3,5 @@
 
 void print_info(){
     kprintf("\n%s %d.%d.%d_%s\n", name, version, build_maj, build_min, state);
-    kprintf("Made by Ayush Yadav. Copyright 2021.\nDistributed under GNU GPLv2.\n\n");
+    kprintf("Made by Ayush Yadav. Copyright 2021.\nDistributed under MIT License.\n\n");
 }
